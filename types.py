dcfeba1bc7cb3f58158638b2c655797df228953a
akhilaadumulla <116962325+akhilaadumulla@users.noreply.github.com>
2022-11-18 22:44:03 +0530

    Add files via upload

---
+++ types.py
@@ -0,0 +1,46 @@
+# """print types in python """
+# # print("Hello world")
+
+# # print('Hello world')
+
+# print("pogram\
+#     on 10th Dec")
+
+# print("""Hello
+# welcome to
+# programming learning""")
+
+
+# print('''hello welcome to
+# programming learning''')
+
+# print("""hello welcome to
+# programming learning""")
+# print("Python"
+# "2 nd floor")
+# print("Josh"),print("josh innovations")
+# print("Josh");print("josh innovations")
+# print("Hello",end="")
+# print("Hello",end=" ")
+# print("Hello",end=",")
+# print("Hello",end=".")
+# print("core","python",end="" "hello")
+# print("core","python",end=" " "hello")
+# print("core","python",end="-" '3')
+
+# '''Note:The above output is
+# core pythonhellocore python hellocore python-3'''
+# print("core","python",sep="-" "hello")
+# print("core","python",end=" " "hello")
+# print("core","python",end="-" "3")
+
+# print("core","python",sep=" ",end="hello")
+# print("core","python",sep="-",end="hello")
+# print("core","python",sep=" ", end="3")
+
+# print("core","python",end="hello",sep=" ")
+# print("core","python",end=" " "hello",sep=" ")
+# print("core","python",end="-3",sep=" ")
+
+# print("Hello"*10)
+# print("hello\n"*10,end="")
